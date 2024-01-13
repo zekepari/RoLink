22d76678ae4949ff607d8e370474fbb655aa2c51
Zeke Pari <pariezekiel@gmail.com>
2024-01-13 19:12:50 +1000

    app test

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import { Client, DiscordAPIError, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
+import express from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]})
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const app = express();
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.username}`)
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+app.listen(3000, () => {
+    console.log('hello')
+})
